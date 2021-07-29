d72afcc303f1775118c143ed19ea634b162b3214
LogicalAlmond <logicalalmond@protonmail.com>
2021-07-29 01:26:23 -0400

    Changes to html and app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
-from flask.templating import render_template
+from flask.templating import send_file, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/downloads')
+def download_file():
+    path = "my_file"
+    return send_file(path, as_attachment=True)
